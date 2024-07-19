940051b1c6f8c241453fc041ee37017ca7c0879c
Vojtěch Naar <vojtechnaar@Vojtechs-MacBook-Pro-2.local>
2024-07-19 09:07:01 +0200

    a

--- test1.py
+++ test1.py
@@ -12,4 +12,10 @@ Haf = Dog("Sandy", 20)
 
 Haf.tell_name("Ahoj")
 
+fridge_contents = {"egg":12, "milk": 2, "apple": 6, "celery": 5}
 
+for variable1, variable2 in fridge_contents.items():
+    if variable1 in fridge_contents:
+        print(fridge_contents[variable1])
+    if variable2 in fridge_contents:
+        print(fridge_contents[variable2])
